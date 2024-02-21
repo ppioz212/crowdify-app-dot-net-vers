b614507407f3c525df2c84b471831f9641b677ec
ppioz212 <adityasuresh25@gmail.com>
2024-02-20 19:19:35 -0800

    added a few new tables

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -42,11 +42,40 @@ CREATE TABLE campaign (
     --CONSTRAINT valid_dates_end_after_start
     --    CHECK (EXTRACT (EPOCH FROM end_date) - EXTRACT (EPOCH FROM start_date) >= 86400),
     CONSTRAINT locked_if_deleted
-        CHECK (deleted <> 1 OR locked <> 1)
+        CHECK (deleted <> 1 OR locked = 1)
+);
+
+CREATE TABLE campaign_manager (
+    campaign_id integer,
+    manager_id integer,
+    creator BIT DEFAULT 1 NOT NULL, --TODO: add constraint only non-creator managers can be removed, also if creator already exists default FALSE
+
+    CONSTRAINT pk_campaign_manager PRIMARY KEY (campaign_id, manager_id),
+    CONSTRAINT fk_campaign_id FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id),
+    CONSTRAINT fk_manager_user_id FOREIGN KEY (manager_id) REFERENCES users (user_id)
+);
+
+
+CREATE TABLE donation (
+    donation_id int IDENTITY(1,1) NOT NULL,
+    donor_id integer,
+    campaign_id integer NOT NULL,
+    donation_amount integer NOT NULL,
+    donation_date datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    donation_comment varchar(200),
+    refunded BIT DEFAULT 0 NOT NULL,
+    anonymous BIT NOT NULL,
+
+    CONSTRAINT pk_donation_id PRIMARY KEY (donation_id),
+    CONSTRAINT fk_campaign_id_2 FOREIGN KEY (campaign_id) REFERENCES campaign (campaign_id),
+    CONSTRAINT fk_donor_user_id FOREIGN KEY (donor_id) REFERENCES users (user_id),
+    CONSTRAINT valid_donation_amount CHECK (donation_amount > 0 AND donation_amount <= 50000000),
+    CONSTRAINT anonymous_if_null_donor_id CHECK (donor_id IS NOT null OR anonymous = 1)
 );
 
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
+
 
 GO
